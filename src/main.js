d482b9478de02eca97e9eb3865ccc9d47d31843c
dxxlx <690305622@qq.com>
2018-08-12 14:59:55 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styless/reset.css'
+import './assets/styless/border.css'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
