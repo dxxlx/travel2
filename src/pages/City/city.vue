34aebc4726702c077641830d9094d8419e5b9fbc
dxxlx <690305622@qq.com>
2018-08-13 17:43:24 +0800

    city ajax

--- src/pages/City/city.vue
+++ src/pages/City/city.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hotCities="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 <script>
@@ -11,6 +11,7 @@
   import CitySearch from './component/search.vue'
   import CityList from './component/List.vue'
   import CityAlphabet from './component/Alphabet.vue'
+  import axios from 'axios'
   export default {
     name:'City',
     components:{
@@ -18,6 +19,29 @@
       CitySearch,
       CityList,
       CityAlphabet
+    },
+    data(){
+      return{
+        cities:{},
+        hotCities:[]
+      }
+    },
+    methods:{
+      getCityInfo(){
+        axios.get('/api/city.json')
+          .then(this.getCityInfoSucc)
+      },
+      getCityInfoSucc(res){
+        res=res.data
+        if(res.ret&&res.data){
+          const data=res.data
+          this.cities=data.cities
+          this.hotCities=data.hotCities
+        }
+      }
+    },
+    mounted(){
+      this.getCityInfo()
     }
   }
 </script>
