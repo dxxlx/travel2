f68a9ce3bef4f5f8b6c8d9ab2b72f2aa61fea693
dxxlx <690305622@qq.com>
2018-08-13 16:12:44 +0800

    city router

---
+++ src/pages/City/city.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <city-header></city-header>
+  </div>
+</template>
+<script>
+  import CityHeader from './component/header.vue'
+  export default {
+    name:'City',
+    components:{
+      CityHeader
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/pages/home/Home'
-
-
+import City from '@/pages/City/City'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'Hello',
       component: Hello
-    }
-  ]
+    },{
+      path: '/city',
+      name: 'Hello',
+      component: City
+    }]
 })
