e0f002d4c5bbb6133333b3f737d96737abed298d
dxxlx <690305622@qq.com>
2018-08-13 17:13:19 +0800

    finish city css

--- src/pages/City/city.vue
+++ src/pages/City/city.vue
@@ -2,16 +2,22 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 <script>
   import CityHeader from './component/header.vue'
   import CitySearch from './component/search.vue'
+  import CityList from './component/List.vue'
+  import CityAlphabet from './component/Alphabet.vue'
   export default {
     name:'City',
     components:{
       CityHeader,
-      CitySearch
+      CitySearch,
+      CityList,
+      CityAlphabet
     }
   }
 </script>
---
+++ src/pages/City/component/Alphabet.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="list">
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+    </ul>
+</template>
+<script>
+    export default {
+        name: 'CityAlphabet'
+    }
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl";
+  .list
+    position:absolute
+    top:1.58rem
+    right:0rem
+    bottom:0rem
+    width:.4rem
+    display:flex
+    flex-direction:column
+    justify-content:center
+    .item
+      line-height:.4rem
+      text-align:center;
+      color:$bgColor
+</style>
